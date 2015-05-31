440d2f705ccb63dc7fad3f4e594937ca163112db
pjsinco <psinco@fas.harvard.edu>
2015-05-31 10:01:15 -0500

    get off-canvas nav to start to work

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,17 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-compass");
   grunt.loadNpmTasks("grunt-contrib-jade");
+  grunt.loadNpmTasks("grunt-autoprefixer");
+  grunt.loadNpmTasks("grunt-contrib-copy");
 
 
   grunt.initConfig({
+    
+    autoprefixer: {
+        css: {
+            src: './**/*.css' 
+        }
+    },
 
     compass: {
       dev: {
@@ -30,6 +38,21 @@ module.exports = function(grunt) {
       }
     },
 
+    copy: {
+        js: {
+            options: {
+
+            },
+            files: [{
+                expand: true,
+                cwd: 'components/js',
+                src: ['**/*.js'],
+                dest: 'builds/dev/js',
+                ext: '.js'
+            }]
+        }
+    },
+
     watch: {
       options: {
         livereload: true
@@ -37,17 +60,23 @@ module.exports = function(grunt) {
       
       sass: {
         files: ['components/sass/**/*.scss'],
-        tasks: ['compass:dev'] 
+        tasks: ['compass:dev', 'autoprefixer:css'] 
       },
 
       compileHtml: {
         files: ['components/jade/*.jade'],
         tasks: ['jade']
+      },
+    
+      copyJs: {
+          files: ['components/js/**/*.js'],
+          tasks: ['copy:js']
       }
     
     } // watch
   }); // initConfig
   
+  grunt.registerTask('copy-js', ['copy:js']);
   grunt.registerTask('default', ['watch']);
 
 }; // exports
--- README.md
+++ README.md
@@ -4,5 +4,12 @@
 * Filament Group [A Responsive Design Approach for Navigation, Part 1](http://www.filamentgroup.com/lab/responsive-design-approach-for-navigation.html)
     * [Demo](http://www.filamentgroup.com/examples/rwd-nav-patterns/)
 * Google Developers: [Web Fundamentals](https://developers.google.com/web/fundamentals/?hl=en)
+* Demo for our nav menu: [demo](https://googlesamples.github.io/web-fundamentals/samples/layouts/navigation-patterns/appbar-navdrawer-sample1.htm://googlesamples.github.io/web-fundamentals/samples/layouts/navigation-patterns/appbar-navdrawer-sample1.html)
 
 * Generated gotham web fonts at fontsquirrel
+
+
+##Sun May 31 06:25:46 2015 CDT
+* Smashing: [Implementing Off-Canvas Navigation For A Responsive Website](http://www.smashingmagazine.com/2013/01/15/off-canvas-navigation-for-responsive-website/)
+
+
